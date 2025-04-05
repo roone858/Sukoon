527870cebc0bfc39829f4b7573547744cd88a009
roone858 <roone858@gmail.com>
2025-04-05 21:24:42 +0200

    other dimensions avaibale when add product

--- src/pages/AddProduct/components/constants.ts
+++ src/pages/AddProduct/components/constants.ts
@@ -20,5 +20,12 @@ export const AVAILABLE_SIZES = [
 export const AVAILABLE_DIMENSIONS = [
      { value: '200x200', label: '٢٠٠×٢٠٠ سم', width: 200, height: 200 },
      { value: '180x200', label: '١٨٠×٢٠٠ سم', width: 180, height: 200 },
+     { value: "180×190", label: "١٨٠×١٩٠ سم", width: 180, height: 190 },
+     { value: "200×160", label: "٢٠٠×١٦٠ سم", width: 160, height: 200 },
+     { value: "200×150", label: "٢٠٠×١٥٠ سم", width: 150, height: 200 },
+     { value: "200×140", label: "٢٠٠×١٤٠ سم", width: 140, height: 200 },
+     { value: "200×120", label: "٢٠٠×١٢٠ سم", width: 120, height: 200 },
+     { value: "190×120", label: "١٩٠×١٢٠ سم", width: 120, height: 190 },
+     { value: "100×200", label: "١٠٠×٢٠٠ سم", width: 100, height: 200 },
      // ... other dimensions
    ];
